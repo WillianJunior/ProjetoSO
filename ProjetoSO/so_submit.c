3377290bd6ee42c340ac1bc2c85f391cd33351e0
Alexandre Lucchesi <alexandrelucchesi@gmail.com>
2013-06-13 20:01:28 -0300

    Ryu.

--- ProjetoSO/so_submit.c
+++ ProjetoSO/so_submit.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include "basic_types.h"
+
+#include "so_submit.h"
 
 typedef struct process process;
 
 int main(int argc, char *argv[]) {
 
- //   FILE *fp;
-  //  struct process x;
+    FILE *fp;
+    struct process x;
 
     if(argc < 2) {
         fprintf(stderr, "Usage: so_submit <process file>.\n");
         exit(1);
     }
-
-//    parse
-
 
 
     
