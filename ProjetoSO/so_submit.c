b6d0a8059acc386b50044819b53286e0216be12b
Alexandre Lucchesi <alexandrelucchesi@gmail.com>
2013-06-13 20:26:23 -0300

    Vegeta.

--- ProjetoSO/so_submit.c
+++ ProjetoSO/so_submit.c
@@ -2,20 +2,44 @@
 #include <stdlib.h>
 #include "so_submit.h"
 
+#define MAX_LINE_LENGTH 100
+
 typedef struct process process;
+
+int parse_process_list(struct process** p_list, const FILE* fp) {
+    char str[MAX_LINE_LENGTH];
+
+    fgets(&str, MAX_LINE_LENGTH, fp);
+
+
+
+}
 
 int main(int argc, char *argv[]) {
 
- //   FILE *fp;
-    struct process x[10];
+    // Alexandre's variables
     FILE *fp;
+    process *p_list;
+    const char* filename;
+    int status;
 
+    // Willian's variables
+    process x[10];
+    
     if(argc < 2) {
         fprintf(stderr, "Usage: so_submit <process file>.\n");
         exit(1);
     }
 
-//    parse
+    filename = argv[1];
+
+    fp = fopen(filename);
+    if(!fp) {
+        fprintf(stderr, "Can't open specified file: <%s>.\n", filename);
+        exit(1);
+    }
+
+    status = parse_process_list(fp, &p_list);
 
     if ((idshm = shmget(0x1223, sizeof(int),IPC_CREAT|0x1ff)) < 0) { 
     	printf("erro na criacao da fila\n"); exit(1);
